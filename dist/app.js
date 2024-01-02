5ed3cfc2fb73f10c76f359e16a772486dd96232a
LeahESc <leahemily1@gmail.com>
2024-01-02 13:00:10 -0800

    Set up project list class

--- dist/app.js
+++ dist/app.js
@@ -36,6 +36,27 @@ const autoBind = (_, _2, descriptor) => {
     };
     return adjDescriptor;
 };
+// project list class
+class ProjectList {
+    constructor(type) {
+        this.type = type;
+        this.templateElement = document.getElementById("project-list");
+        this.hostElement = document.getElementById("app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.element.id = `${type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector('ul').id = listId;
+        this.element.querySelector('h2').textContent = this.type.toUpperCase() + 'PROJECTS';
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
+    }
+}
 // project input class
 class ProjectInput {
     constructor() {
@@ -116,4 +137,6 @@ class ProjectInput {
 __decorate([
     autoBind
 ], ProjectInput.prototype, "submitHandler", null);
-const project = new ProjectInput();
+const projectInput = new ProjectInput();
+const activeProjectList = new ProjectList('active');
+const finishedProjectList = new ProjectList('finished');
